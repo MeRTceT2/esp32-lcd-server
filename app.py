6b7452332c2db3900d9270363a7cc923bf82e1ef
MeRTceT2 <121510283+MeRTceT2@users.noreply.github.com>
2025-07-23 19:27:14 +0300

    Update app.py

--- app.py
+++ app.py
@@ -4,28 +4,28 @@ app = Flask(__name__)
 last_message = "Hazir"
 
 @app.route('/')
-def home():
-    return f"""
-    <h2>LCD Ekran Mesajı:</h2>
-    <p>{last_message}</p>
-    <form action="/send" method="post">
-        <input type="text" name="message" placeholder="Mesaj yaz" required>
-        <button type="submit">Gönder</button>
-    </form>
-    """
-
-@app.route('/send', methods=['POST'])
-def send():
-    global last_message
-    if request.form.get('message'):
-        last_message = request.form.get('message')
-    else:
-        last_message = request.data.decode('utf-8')
-    return "OK"
-
-@app.route('/text', methods=['GET'])
-def text():
+def get_message():
     return last_message
+
+@app.route('/send', methods=['GET', 'POST'])
+def post_message():
+    global last_message
+    if request.method == 'POST':
+        last_message = request.data.decode('utf-8')
+        return "OK"
+    else:
+        return '''
+            <form method="POST">
+                <input name="msg" placeholder="Mesaj yaz" required>
+                <button type="submit">Gönder</button>
+            </form>
+        '''
+
+@app.route('/send-form', methods=['POST'])
+def send_form():
+    global last_message
+    last_message = request.form.get('msg')
+    return f"Mesaj gönderildi: {last_message}<br><a href='/send'>Geri dön</a>"
 
 if __name__ == '__main__':
     app.run()
